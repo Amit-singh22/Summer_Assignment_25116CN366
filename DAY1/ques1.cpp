ad6d087665f0e24c217bfc464213826b5a6ccc07
Amit-singh22 <amitrajpali97@gmail.com>
2026-05-28 19:11:59 +0530

    Day 1 assignment

---
+++ DAY1/ques1.cpp
@@ -0,0 +1,17 @@
+// Write a program to calculate sum of first N natural number.
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int N,sum=0;
+
+    cout<<"Enter a number:";
+    cin>>N;
+
+    for(int i=1;i<=N;i++){
+        sum +=i;
+    }
+
+    cout<<"Sum="<<sum;
+}
